7f11d9f628bb1966a26eba769a99d44d82dc7a02
Risna Putri Ramadhani <54300011+risnaardn@users.noreply.github.com>
2022-09-01 15:01:55 +0700

    Add files via upload

---
+++ orangeHRMlogin.py
@@ -0,0 +1,30 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestLogin(unittest.TestCase):  # TEST SCENARIO
+
+    def setUp(self): 
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        
+    def test_a_success_login(self): 
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") # buka situs
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input").send_keys("admin") # isi email
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123") # isi password
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol sign in
+        time.sleep(2)
+        browser.get("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList") # buka situs
+        time.sleep(3)
+
+    def tearDown(self): 
+        self.browser.close() 
+
+if __name__ == "__main__": 
+    unittest.main()
